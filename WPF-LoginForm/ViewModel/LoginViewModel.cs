2c50adc43053f02c96b533b411e2e57b6d65f086
Patrick Hübner <phuebner86@gmail.com>
2022-08-15 21:37:04 +0200

    Implement ViewModel

---
+++ WPF-LoginForm/ViewModel/LoginViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_LoginForm.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        // Fields
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        public string Username 
+        {
+            get => _username;
+
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public SecureString Password
+        {
+            get => _password;
+            
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage
+        {
+            get => _errorMessage;
+            
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisible
+        {
+            get => _isViewVisible;
+            
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelCommand(p =>ExecuteRecoverPasswordCommand("", ""));
+        }
+
+
+        private bool CanExecuteLoginCommand(object? arg)
+        {
+            if(string.IsNullOrWhiteSpace(Username) || Username.Length < 3 || 
+                Password == null || Password.Length < 3)
+                return false;
+            
+
+            return true;
+        }
+
+        private void ExecuteLoginCommand(object? obj)
+        {
+            throw new NotImplementedException();
+        }
+        private void ExecuteRecoverPasswordCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPF-LoginForm/ViewModel/ViewModelCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_LoginForm.ViewModel
+{
+    public class ViewModelCommand : ICommand
+    {
+        // Fields
+        private readonly Action<object?> _executeAction;
+        private readonly Func<object?, bool>? _canExecute;
+
+        // Constructors
+        public ViewModelCommand(Action<object?> executeAction)
+        {
+            _executeAction = executeAction;
+            _canExecute = null;
+        }
+
+        public ViewModelCommand(Action<object?> executeAction, Func<object?, bool> canExecuteAction)
+        {
+            _executeAction = executeAction;
+            _canExecute = canExecuteAction;
+        }
+
+        // Events
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; } 
+        }
+
+        // Methods
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _executeAction(parameter);
+        }
+    }
+}
